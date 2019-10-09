51c021be01d9da516db22b83cf1ffe1b969cccf5
aleibo92 <aleibo92@gmail.com>
2019-10-09 21:58:35 +0000

    lesson 17

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,8 +5,10 @@ factory :user do
     password_confirmation { 'secretPassword' }
   end
 
-  factory :gram do
-    message { "hello" }
-    association :user
-  end
+factory :gram do
+  message { "hello" }
+  picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
+
+  association :user
+end
 end
